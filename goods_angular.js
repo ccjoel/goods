7dad9c7d5a3d5843cd4256b88091abb8868e89f1
Joel Quiles Baker <quilesbaker@gmail.com>
2015-01-12 15:52:49 -0500

    another practice i have to remember when using AngularJs

--- goods_angular.js
+++ goods_angular.js
@@ -16,3 +16,48 @@ app.controller('MainCtrl', ['$scope', '_', function($scope, _) {
 }]);
 
 //Read more at http://www.airpair.com/angularjs/posts/top-10-mistakes-angularjs-developers-make#TWPDLE5rqJOVgMli.99
+
+
+
+/////////
+
+// DIrectives with Controller As Syntax:
+
+// using: <div my-example max="77"></div>
+angular
+    .module('app')
+    .directive('myExample', myExample);
+
+function myExample() {
+    var directive = {
+        restrict: 'EA',
+        templateUrl: 'app/feature/example.directive.html',
+        scope: {
+            max: '='
+        },
+        link: linkFunc,
+        controller: ExampleController,
+        controllerAs: 'vm'
+    };
+
+    return directive;
+
+    function linkFunc(scope, el, attr, ctrl) {
+        console.log('LINK: scope.max = %i', scope.max);
+        console.log('LINK: scope.vm.min = %i', scope.vm.min);
+        console.log('LINK: scope.vm.max = %i', scope.vm.max);
+    }
+}
+
+ExampleController.$inject = ['$scope'];
+
+function ExampleController($scope) {
+    // Injecting $scope just for comparison
+    var vm = this;
+
+    vm.min = 3; 
+    vm.max = $scope.max; 
+    console.log('CTRL: $scope.max = %i', $scope.max);
+    console.log('CTRL: vm.min = %i', vm.min);
+    console.log('CTRL: vm.max = %i', vm.max);
+}
